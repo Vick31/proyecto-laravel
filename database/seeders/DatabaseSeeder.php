d6440590398458ee8ac553fa267c0b4d4b82eb99
Manuel Stiven Pérez Victoria <89532383+Vick31@users.noreply.github.com>
2022-09-30 08:56:47 -0500

    actualizacion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\companies;
+use App\Models\roles;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,15 +18,36 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([
-            EquipmentSeeder::class,
-            ReportsSeeder::class,
-            RolesSeeder::class,
-            CompaniesSeeder::class,
-            ClientSeeder::class,
-            UserSeeder::class,
-            EventesSeeder::class
-        ]);
 
+        roles::insert(
+            [
+                'name' => 'admin',
+                'is_admin' => 1
+            ]
+        );
+
+        companies::insert(
+            [
+                'name' => 'companies admin',
+                'phone_number' => 123,
+                'address' => 'admin123',
+                'email' => 'admins@ad.com',
+                'created_at' => date("Y/m/d"),
+            ]
+        );
+
+
+        User::insert(
+            [
+                'roles_id' =>  1,
+                'companies_id' =>  1, //admin role id
+                'name' =>  'admin', //default name
+                'dni' => 1,
+                'phone_number' =>  1,
+                'email' => 'admin@admin.com', //default email
+                'password' => Hash::make('admin123'), //default admin password: admin123
+                'created_at' => date("Y/m/d"), //created today
+            ]
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -39,3 +39,4 @@ Route::post('reset-password', [NewPasswordController::class, 'reset']);
 Route::resource('/clientes', Clientcontroller::class);
 Route::resource('/roles', RolesController::class);
 Route::resource('/citas', EventsController::class);
+
